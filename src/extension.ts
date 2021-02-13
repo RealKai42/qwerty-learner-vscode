50fc000b441401eeed5ce1f94d204b63d832ffb7
KaiyiWing <Zhang.Kaiyi42@gmail.com>
2021-02-14 02:11:07 +0800

    feat(word): 完成基本的单词输入和翻译显示的功能

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,97 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+import { off } from 'process'
 import * as vscode from 'vscode'
+import cet4 from './assets/dicts/CET4_T.json'
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+function compareWord(word: string, input: string) {
+  // 错误返回错误索引，正确返回-2，未完成输入且无错误返回-1
+  // console.log(word, input)
+  for (let i = 0; i < word.length; i++) {
+    if (typeof input[i] !== 'undefined') {
+      if (word[i] !== input[i]) {
+        return i
+      }
+    } else {
+      return -1
+    }
+  }
+  return -2
+}
+
+function replaceString(str: string, index: number, char: string) {
+  return str.substring(0, index) + char + str.substring(index + 1)
+}
+function highlightError(str: string, errorIndex: number) {
+  return `${str.substring(0, errorIndex)} _${str[errorIndex]}_ ${str.substring(errorIndex + 1)}`
+}
+
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "qwerty-learner" is now active!')
+  const chapterLength = 20
+  let isStart = false,
+    hasWrong = false,
+    chapter = 0,
+    order = 0,
+    dict = cet4
+  let wordList = dict.slice(chapter * chapterLength, (chapter + 1) * chapterLength)
+  const wordBar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, -100)
+  const inputBar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, -101)
+  const transBar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, -102)
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand('qwerty-learner.helloWorld', () => {
-    // The code you place here will be executed every time your command is executed
+  const setupWord = () => {
+    if (order === chapterLength - 1) {
+      chapter += 1
+      order = 0
+      wordList = dict.slice(chapter * chapterLength, (chapter + 1) * chapterLength)
+    }
+    wordBar.text = `chp.${chapter}  ${order}/${chapterLength}  ${wordList[order].name}`
+    inputBar.text = ''
+    transBar.text = wordList[order].trans.join('；')
+  }
 
-    // Display a message box to the user
-    vscode.window.showInformationMessage('Hello World from Qwerty Learner!')
+  vscode.workspace.onDidChangeTextDocument((e) => {
+    if (isStart) {
+      const { uri } = e.document
+      const { range, text, rangeLength } = e.contentChanges[0]
+      if (rangeLength === 0) {
+        // 删除用户输入的字符
+        const newRange = new vscode.Range(range.start.line, range.start.character, range.end.line, range.end.character + 1)
+        const editAction = new vscode.WorkspaceEdit()
+        editAction.delete(uri, newRange)
+        vscode.workspace.applyEdit(editAction)
+
+        if (!hasWrong && text.length === 1) {
+          inputBar.text += text
+          const result = compareWord(wordList[order].name, inputBar.text)
+          if (result === -2) {
+            order++
+            setupWord()
+          } else if (result >= 0) {
+            hasWrong = true
+            inputBar.text = highlightError(inputBar.text, result)
+            setTimeout(() => {
+              hasWrong = false
+              setupWord()
+            }, 500)
+          }
+        }
+      }
+    }
   })
 
-  context.subscriptions.push(disposable)
+  let start = vscode.commands.registerCommand('qwerty-learner.Start', () => {
+    isStart = !isStart
+    if (isStart) {
+      wordBar.show()
+      inputBar.show()
+      transBar.show()
+      setupWord()
+    } else {
+      wordBar.hide()
+      inputBar.hide()
+      transBar.hide()
+    }
+  })
+
+  context.subscriptions.push(start)
 }
 
 // this method is called when your extension is deactivated
